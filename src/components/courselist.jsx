ef8c42a5049a6c6f8edb136a3c159429d1eedf91
Jasmine Meyer <jasminemeyer000@gmail.com>
2023-10-04 01:05:03 -0500

    anutha one 3

--- src/components/courselist.jsx
+++ src/components/courselist.jsx
@@ -3,13 +3,15 @@ import React from "react";
 function Courselist({ courses, prm1, prm2, prm3 }) {
   const courseElems = [];
 
-  for (let course in courses) {
-    const prmx = course[{ prm1 }];
-    const prmy = course[{ prm2 }];
-    const prmz = course[{ prm3 }];
+  for (let ckey in courses) {
+    //loop through different string keys
+    const course = courses[ckey];
+    const prmx = course[prm1];
+    const prmy = course[prm2];
+    const prmz = course[prm3];
 
     const courseElem = (
-      <p key={course}>
+      <p key={ckey}>
         {prmx} CS {prmy}: {prmz}
       </p>
     );
